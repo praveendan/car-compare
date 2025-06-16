eb1d4648d38450df47bb2885bc2831ca8f2a083b
praveendan <38735067+praveendan@users.noreply.github.com>
2025-06-15 20:55:32 -0500

    added theme selector

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,13 +2,59 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Comparer from './comparer/Comparer';
 import { GlobalProvider } from '../context/GlobalProvider';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+
+type ThemeType = 'light' | 'dark'
 
 const Dashboard = () => {
+  const [theme, setTheme] = useState<ThemeType>('light')
+
+  useEffect(() => {
+    //System dark/light mode changes 
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    /**
+     * Setting the saved theme
+     * priority is given to the saved one, if there's no saved one, system default is applied
+     */
+    const savedTheme = localStorage.getItem('theme')
+    if (savedTheme) {
+      setTheme(savedTheme as ThemeType)
+    } else {
+      setTheme(mediaQuery.matches ? 'dark' : 'light');
+    }
+
+    //Listen to System dark/light mode changes 
+    const handleChange = () => {
+      setTheme(mediaQuery.matches ? 'dark' : 'light');
+    };
+
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, [])
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+  }, [theme]);
+
+  const updateTheme = () => {
+    if (theme === 'dark') {
+      localStorage.setItem('theme', 'light')
+      setTheme('light')
+    } else {
+      localStorage.setItem('theme', 'dark')
+      setTheme('dark')
+    }
+  }
+
   return (
     <GlobalProvider>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="#home">Car-Compare</Navbar.Brand>
+          <FontAwesomeIcon icon={theme === 'dark' ? faSun : faMoon} onClick={updateTheme} role='button' title='Toggle Dark/Light mode'/>
         </Container>
       </Navbar>
       <Comparer />
